585b0d9eff06e066152f853b9835cf80904043c2
Oussama Jarrousse <oussama@jarrousse.org>
2023-04-10 05:58:08 +0200

    fixes

--- uwazi/docker-entrypoint.sh
+++ uwazi/docker-entrypoint.sh
@@ -1,19 +1,13 @@
-#!/bin/sh
-
-set -Ee
+#!/bin/bash
 
 # evaluate first run or not first run
 DB_URL="mongodb://$DBHOST:27017"
-DB_DOES_NOT_EXISTS=$(mongosh --quiet $DB_URL --eval "db.getMongo().getDBNames().indexOf('$DATABASE_NAME') == -1" | grep -q true)
-
-if $DB_DOES_NOT_EXISTS; then
+DB_DOES_NOT_EXISTS=$(mongosh --quiet $DB_URL --eval "db.getMongo().getDBNames().indexOf('$DATABASE_NAME') == -1"
+if [ $DB_DOES_NOT_EXISTS = "true" ]; then
 	echo "uwazi-docker: MongoDB and Elastic Search have not been initialized yet."
 	# NODE_ENV=production yarn blank-state ${DATABASE_NAME}
 	yarn blank-state ${DATABASE_NAME}
 fi
-
 # RUN mkdir -p ./uwazi/log ./uwazi/uploaded_documents ./uwazi/custom_uploads ./uwazi/temporal_files
-
-
 # DATABASE_NAME=my_db_name INDEX_NAME=my_db_name NODE_ENV=production FILES_ROOT_PATH=/xxxx/yyyy/uwazi/ node server.js
-node server.js
+exec node server.js
